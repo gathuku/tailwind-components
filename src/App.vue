2c88cb3bec3c979827f5fbde13749388376ac9d1
gathuku <mosesgathuku95@gmail.com>
2020-03-25 01:06:44 +0300

    blog layout

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,18 @@
 <template>
   <div id="app">
-    <div class="text-center">      
-      <span class="font-extrabold text-3xl text-gray-900">Hello world</span>
+    <div class="text-center">
+     <blog-layout></blog-layout>
     </div>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
-
+import BlogLayout from './components/BlogLayout.vue'
 export default {
   name: 'App',
   components: {
-
+    BlogLayout,
   }
 }
 </script>
---
+++ src/components/BlogLayout.vue
@@ -0,0 +1,128 @@
+<template id="">
+  <div class="bg-gray-100 py-16 px-4">
+    <div class="text-center">
+      <h2 class="text-4xl leading-10 tracking-tight font-extrabold text-gray-900">
+        From the Blog
+      </h2>
+      <p class="mt-5 text-xl leading-7 text-gray-500">
+        Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+      </p>
+    </div>
+
+    <div class="mt-12 text-left flex flex-col md:flex-row gap-5 max-w-lg mx-auto md:grid-col-3 lg:max-w-none">
+      <div class="rounded-lg shadow-lg overflow-hidden">
+         <div>
+           <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+         </div>
+         <div class="p-6">
+           <p class="text-sm leading-5 font-medium text-indigo-500">
+             Blog
+           </p>
+           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+             Introduction to ruby on Rails
+           </h3>
+           <p class="mt-3 text-base leading-6 text-gray-500">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+           </p>
+          <div class="mt-6 flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+            </div>
+            <div class="ml-3 ">
+              <p class="text-sm leading-5 font-medium text-gray-800">
+                Paul York
+              </p>
+              <div class="flex text-sm leading-5 text-gray-500">
+                <time datetime="2020-03-24">
+                  Mar 24, 2020
+                </time>
+                <span>
+                  &middot;
+                </span>
+                <span>
+                  6 min read
+                </span>
+              </div>
+            </div>
+          </div>
+         </div>
+      </div>
+
+      <div class="rounded-lg shadow-lg overflow-hidden">
+         <div>
+           <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+         </div>
+         <div class="p-6">
+           <p class="text-sm leading-5 font-medium text-indigo-500">
+             Blog
+           </p>
+           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+             Introduction to ruby on Rails
+           </h3>
+           <p class="mt-3 text-base leading-6 text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+           </p>
+          <div class="mt-6 flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+            </div>
+            <div class="ml-3 ">
+              <p class="text-sm leading-5 font-medium text-gray-800">
+                Paul York
+              </p>
+              <div class="flex text-sm leading-5 text-gray-500">
+                <time datetime="2020-03-24">
+                  Mar 24, 2020
+                </time>
+                <span>
+                  &middot;
+                </span>
+                <span>
+                  6 min read
+                </span>
+              </div>
+            </div>
+          </div>
+         </div>
+      </div>
+
+      <div class="rounded-lg shadow-lg overflow-hidden">
+         <div>
+           <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+         </div>
+         <div class="p-6">
+           <p class="text-sm leading-5 font-medium text-indigo-500">
+             Blog
+           </p>
+           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+             Introduction to ruby on Rails
+           </h3>
+           <p class="mt-3 text-base leading-6 text-gray-500">
+             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt            </p>
+          <div class="mt-6 flex items-center">
+            <div class="flex-shrink-0">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+            </div>
+            <div class="ml-3 ">
+              <p class="text-sm leading-5 font-medium text-gray-800">
+                Paul York
+              </p>
+              <div class="flex text-sm leading-5 text-gray-500">
+                <time datetime="2020-03-24">
+                  Mar 24, 2020
+                </time>
+                <span>
+                  &middot;
+                </span>
+                <span>
+                  6 min read
+                </span>
+              </div>
+            </div>
+          </div>
+         </div>
+      </div>
+    </div>
+
+  </div>
+</template>
