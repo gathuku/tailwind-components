dee1985e2949d6a76ce076032dd2619d56e581d3
gathuku <mosesgathuku95@gmail.com>
2020-03-25 22:29:03 +0300

    update

--- src/components/BlogLayout.vue
+++ src/components/BlogLayout.vue
@@ -1,36 +1,38 @@
 <template id="">
-  <div class="bg-gray-100 py-16 px-4">
+  <div class="bg-gray-100 py-16 px-4 md:px-6">
     <div class="text-center">
       <h2 class="text-4xl leading-10 tracking-tight font-extrabold text-gray-900">
         From the Blog
       </h2>
-      <p class="mt-5 text-xl leading-7 text-gray-500">
+      <p class="mt-5 text-xl leading-7 text-gray-500 max-w-2xl md:mx-w-none mx-auto">
         Lorem ipsum dolor sit amet sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
       </p>
     </div>
 
-    <div class="mt-12 text-left grid gap-5 grid-cols-1 md:grid-cols-3 lg:grid-cols-3">
-      <div class="rounded-lg shadow-lg overflow-hidden">
-         <div>
+    <div class="mt-12 text-left grid gap-5 grid-cols-1 md:grid-cols-3 lg:grid-cols-3 max-w-lg mx-auto md:max-w-none">
+      <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+         <div class="flex-shrink-0">
            <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
          </div>
-         <div class="p-6">
-           <p class="text-sm leading-5 font-medium text-indigo-500">
-             Blog
-           </p>
-           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
-             Introduction to ruby on Rails
-           </h3>
-           <p class="mt-3 text-base leading-6 text-gray-500">
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-           </p>
+         <div class="flex-1 flex flex-col justify-between p-6">
+           <div class="flex-shrink-0">
+             <p class="text-sm leading-5 font-medium text-indigo-500">
+               Blog
+             </p>
+             <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+               Setting Tailwind in Vue CLI
+             </h3>
+             <p class="mt-3 text-base leading-6 text-gray-500">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt            </p>
+           </div>
+
           <div class="mt-6 flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
             </div>
             <div class="ml-3 ">
               <p class="text-sm leading-5 font-medium text-gray-800">
-                Paul York
+                Moses Gathuku
               </p>
               <div class="flex text-sm leading-5 text-gray-500">
                 <time datetime="2020-03-24">
@@ -48,27 +50,29 @@
          </div>
       </div>
 
-      <div class="rounded-lg shadow-lg overflow-hidden">
-         <div>
-           <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+      <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+         <div class="flex-shrink-0">
+           <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1556710258-181a10df302d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
          </div>
-         <div class="p-6">
-           <p class="text-sm leading-5 font-medium text-indigo-500">
-             Blog
-           </p>
-           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
-             Introduction to ruby on Rails
-           </h3>
-           <p class="mt-3 text-base leading-6 text-gray-500">
-              Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
-           </p>
+         <div class="flex-1 flex flex-col justify-between p-6">
+           <div class="flex-1">
+             <p class="text-sm leading-5 font-medium text-indigo-500">
+               Blog
+             </p>
+             <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+               Vue router configuration
+             </h3>
+             <p class="mt-3 text-base leading-6 text-gray-500">
+               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt
+             </p>
+           </div>
           <div class="mt-6 flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
             </div>
             <div class="ml-3 ">
               <p class="text-sm leading-5 font-medium text-gray-800">
-                Paul York
+                Moses Gathuku
               </p>
               <div class="flex text-sm leading-5 text-gray-500">
                 <time datetime="2020-03-24">
@@ -86,26 +90,30 @@
          </div>
       </div>
 
-      <div class="rounded-lg shadow-lg overflow-hidden">
-         <div>
+      <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
+         <div class="flex-shrink-0">
            <img class="h-48 w-full object-cover" src="https://images.unsplash.com/photo-1547586696-ea22b4d4235d?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=1080&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
          </div>
-         <div class="p-6">
-           <p class="text-sm leading-5 font-medium text-indigo-500">
-             Blog
-           </p>
-           <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
-             Introduction to ruby on Rails
-           </h3>
-           <p class="mt-3 text-base leading-6 text-gray-500">
-             Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt            </p>
+         <div class="flex-1 flex flex-col justify-between p-6">
+           <div class="flex-shrink-0">
+             <p class="text-sm leading-5 font-medium text-indigo-500">
+               Blog
+             </p>
+             <h3 class="mt-2 text-xl leading-7 font-semibold text-gray-900 ">
+               Setting Tailwind in Vue CLI
+             </h3>
+             <p class="mt-3 text-base leading-6 text-gray-500">
+               Lorem ipsum dolor sit amet
+             </p>
+           </div>
+
           <div class="mt-6 flex items-center">
             <div class="flex-shrink-0">
-              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1544725176-7c40e5a71c5e?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=300&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
+              <img class="h-10 w-10 rounded-full" src="https://images.unsplash.com/photo-1537815749002-de6a533c64db?ixlib=rb-1.2.1&q=80&fm=jpg&crop=entropy&cs=tinysrgb&w=200&fit=max&ixid=eyJhcHBfaWQiOjF9" alt="">
             </div>
             <div class="ml-3 ">
               <p class="text-sm leading-5 font-medium text-gray-800">
-                Paul York
+                Moses Gathuku
               </p>
               <div class="flex text-sm leading-5 text-gray-500">
                 <time datetime="2020-03-24">
